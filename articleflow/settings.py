769c855ca4608d48a16bed1536b14bd6ff07082c
Anzil619 <anzilaz24@gmail.com>
2023-12-11 23:26:45 +0530

    removed enc

--- articleflow/settings.py
+++ articleflow/settings.py
@@ -97,12 +97,7 @@ AUTH_USER_MODEL = 'accounts.CustomUser'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+
 
 DATABASES = {
     'default': {
